73e9a63b75a1d4d04bc5508574ffbe1c22a4d6a1
Darren Tyson - DCC <darren.tyson@duke.edu>
2025-03-19 18:31:35 -0400

    adding scripts to generate UCE for different datasets

---
+++ run-33l_AI_prim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -e slurm.err
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:a6000:1 # 48 GB GPU RAM
+#SBATCH -c 6
+#SBATCH --mem=50G
+
+python eval_single_anndata.py \
+--adata_path /hpc/group/oliverlab/adata_objects/021825_RPM_CGRPvK5_adata3_counts.h5ad \
+--dir /work/drt42/UCE_output/ \
+--species mouse --nlayers 33 \
+--model_loc /hpc/group/oliverlab/UCE/33l_8ep_1024t_1280.torch \
+--batch_size 40 \
+--protein_embeddings_dir /hpc/group/oliverlab/UCE/protein_embeddings
--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@
 #SBATCH -c 6
 #SBATCH --mem=50G
 
-python eval_single_anndata.py --adata_path /hpc/group/oliverlab/lung_atlas/Lin_2024_hillock_QC.h5ad --dir /hpc/home/drt42/work-drt42/UCE --species mouse --model_loc /hpc/group/oliverlab/UCE/4layer_model.torch --batch_size 25
+python eval_single_anndata.py --adata_path /hpc/group/oliverlab/lung_atlas/Lin_2024_hillock_QC.h5ad --dir /hpc/home/drt42/work-drt42/UCE/ --species mouse --model_loc /hpc/group/oliverlab/UCE/4layer_model.torch --batch_size 50
 
