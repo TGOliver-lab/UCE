407c330df7561861664b74014ae98f00dd80e050
Darren Tyson - DCC <darren.tyson@duke.edu>
2025-06-08 23:27:01 -0400

    moving files into scripts-logs

---
+++ scripts-logs/run-33l_hONB_OMIX.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -e slurm.err
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:a6000:1 # 48 GB GPU RAM
+#SBATCH -c 6
+#SBATCH --mem=50G
+
+python eval_single_anndata.py \
+--adata_path /hpc/group/oliverlab/ONB_data/hONB_OMIX004730.h5ad \
+--dir /work/drt42/UCE_output/ \
+--species human --nlayers 33 \
+--model_loc /hpc/group/oliverlab/UCE/33l_8ep_1024t_1280.torch \
+--batch_size 40 \
+--protein_embeddings_dir /hpc/group/oliverlab/UCE/protein_embeddings
---
+++ scripts-logs/run-33l_humanOE.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -e slurm.err
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:a6000:1 # 48 GB GPU RAM
+#SBATCH -c 4
+#SBATCH --mem=90G
+
+python eval_single_anndata.py \
+--adata_path /hpc/group/oliverlab/adata_objects/ONB/humanOE_Goldstein.h5ad \
+--dir /work/drt42/UCE_output/ \
+--species human --nlayers 33 \
+--model_loc /hpc/group/oliverlab/UCE/33l_8ep_1024t_1280.torch \
+--batch_size 40 \
+--protein_embeddings_dir /hpc/group/oliverlab/UCE/protein_embeddings
